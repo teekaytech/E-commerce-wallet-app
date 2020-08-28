d7af4f599caf1f7440f496348195a52a46b8b2a2
teekaytech <taofeek.olalere@feghas.com>
2020-08-28 16:12:56 +0100

    Use error log

--- config/logging.php
+++ config/logging.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
+            'driver' => 'errorlog',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
