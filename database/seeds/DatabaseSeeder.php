e7861dad2282b31176a0acdf77be5446dc7abbd9
teekaytech <taofeek.olalere@feghas.com>
2020-08-28 18:47:54 +0100

    Add seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        {
+            DB::table('customers')->insert([
+                'firstname' => 'Adeleke',
+                'lastname' => 'Alice',
+                'phone_number' => '08139160110',
+                'email' => 'customer1@gmail.com'
+            ]);
+
+            DB::table('customers')->insert([
+                'firstname' => 'Taofeek',
+                'lastname' => 'Olalere',
+                'phone_number' => '07085516354',
+                'email' => 'customer2@gmail.com'
+            ]);
+
+            DB::table('customers')->insert([
+                'firstname' => 'Abiodun',
+                'lastname' => 'Olamilekan',
+                'phone_number' => '09088787665',
+                'email' => 'customer3@gmail.com'
+            ]);
+        }
     }
 }
